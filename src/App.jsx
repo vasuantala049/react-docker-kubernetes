29db4b72cc28e85fb6cedc28936f904137ae8c33
Vasu Antala <vasuantala098@gmail.com>
2025-12-17 07:10:41 +0530

    Remove unused imports from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 
 import styles from "./components/List.module.css"
-import { NetflixSeries } from "./components/NetflixSeries";
+
 import { Todo } from "./components/projects/todo/Todo";
-import { ToggleSwitch } from "./components/projects/toggleswitch";
+
 export const App = () => {
 return (
     <>
